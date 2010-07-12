ada079ed53daf3210f72ee32bf40bbe9223309bb
nate <n8@adullmoment.com>
2010-07-12 17:02:27 -0400

    commented main site.pp

--- site.pp
+++ site.pp
@@ -1,9 +1,11 @@
-import "classes/*.pp"
-
-node default {
-  include sudo
-  include motd
-  include johnny6
-  include srcfolder
-  include apps
-}
+# using manifests/site.pp now
+#
+#import "classes/*.pp"
+#
+#node default {
+#  include sudo
+#  include motd
+#  include johnny6
+#  include srcfolder
+#  include apps
+#}
