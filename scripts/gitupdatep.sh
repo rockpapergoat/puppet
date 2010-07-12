1b1ac5649539e4e75d4fddf17829279e595fcdef
nate <n8@adullmoment.com>
2010-07-12 16:47:09 -0400

    still more hook testing

--- scripts/gitupdatep.sh
+++ scripts/gitupdatep.sh
@@ -2,4 +2,4 @@
 # use git to update /etc/puppet
 git=/usr/local/git/bin/git
 # git pull git@github.com:tspgit/puppet.git master
-$git clone http://github.com/tspgit/puppet.git /etc/puppet
+$git clone http://github.com/tspgit/puppet.git /etc/pconfig/
